692db8c95ece7292e48b6a0624fce87911f9f55b
Vinicius Godoy <vinigodoy@gmail.com>
2012-02-21 17:12:53 -0200

    Test for Vector3 class.

--- test/ForFunTests/Vector3Test.cpp
+++ test/ForFunTests/Vector3Test.cpp
@@ -132,6 +132,10 @@ void testVector3()
 	v1.resize(100);
 	assertEquals(__LINE__, 100.0f, v1.size());	
 
+	v1.set(0,0,0);
+	v1.resize(20);
+	assertEquals(__LINE__, 0.0f, v1.size());	
+
 	//Cross product
 	v1 = Vector3(3, -3, 1);
 	v2 = Vector3(4, 9, 2);
@@ -143,5 +147,118 @@ void testVector3()
 	v2 = Vector3(30, 20, 10);
 	assertEquals(__LINE__, 100, v1.dot(v2));
 
+	//Normalize
+	v1 = Vector3(1, 2, 3);
+	v1.normalize();
+	assertEquals(__LINE__, 1, v1.size());
 
+	v1 = Vector3(0,0,0);
+	v1.normalize();
+	assertEquals(__LINE__, 0, v1.size());
+
+	//Rotate X
+	v1 = Vector3(3,0,1);
+	v1.rotateX(toRadians(90));
+	assertEquals(__LINE__, 3.0f, -1.0f, 0.0f, v1);
+
+	//Rotate Y
+	v1 = Vector3(0,3,1);
+	v1.rotateY(toRadians(90));
+	assertEquals(__LINE__, 1.0f, 3.0f, 0.0f, v1);
+
+	//Rotate Z
+	v1 = Vector3(0,1,3);
+	v1.rotateZ(toRadians(90));
+	assertEquals(__LINE__, -1.0f, 0.0f, 3.0f, v1);
+
+	//RotateAxis
+	v1 = Vector3(3,0,1);
+	v1.rotateAxis(toRadians(90), Vector3(1,0,0));
+	assertEquals(__LINE__, 3.0f, -1.0f, 0.0f, v1);
+
+	v1 = Vector3(0,3,1);
+	v1.rotateAxis(toRadians(90), Vector3(0,1,0));
+	assertEquals(__LINE__, 1.0f, 3.0f, 0.0f, v1);
+
+	v1 = Vector3(0,1,3);
+	v1.rotateAxis(toRadians(90), Vector3(0,0,1));
+	assertEquals(__LINE__, -1.0f, 0.0f, 3.0f, v1);
+
+	//Is Zero
+	v1 = Vector3(0,1,3);
+	assertTrue(__LINE__, !v1.isZero());
+	v1 = Vector3(0,0,0);
+	assertTrue(__LINE__, v1.isZero());
+
+	//Normalize
+	v1 = Vector3(5,3,1);
+	assertTrue(__LINE__, !v1.isUnit());
+	v1.normalize();
+	assertTrue(__LINE__, v1.isUnit());		
+
+	//Functions and operators
+	v1 = Vector3(1.0f, 2.0f, 3.0f);
+	v2 = 10 * v1;
+	assertEquals(__LINE__, 1.0f, 2.0f, 3.0f, v1);
+	assertEquals(__LINE__, 10.0f, 20.0f, 30.0f, v2);
+
+	v1 = Vector3(5.0f, 0.0f, 0.0f);
+	assertTrue(__LINE__, areOrthogonal(v1, Vector3(0.0f, 3.0f, 0.0f)));
+	assertTrue(__LINE__, areOrthogonal(v1, Vector3(0.0f, -3.0f, 0.0f)));
+	assertTrue(__LINE__, !areOrthogonal(v1, Vector3(3.0f, 4.0f, 0.0f)));
+
+	//Rotate X
+	v1 = Vector3(3.0f,0.0f,1.0f);
+	v2 = rotateX(v1, toRadians(90));
+	assertEquals(__LINE__, 3.0f, -0.0f, 1.0f, v1);
+	assertEquals(__LINE__, 3.0f, -1.0f, 0.0f, v2);
+
+	//Rotate Y
+	v1 = Vector3(0.0f, 3.0f, 1.0f);
+	v2 = rotateY(v1, toRadians(90));
+	assertEquals(__LINE__, 0.0f, 3.0f, 1.0f, v1);
+	assertEquals(__LINE__, 1.0f, 3.0f, 0.0f, v2);
+
+	//Rotate Z
+	v1 = Vector3(0.0f, 1.0f, 3.0f);
+	v2 = rotateZ(v1, toRadians(90));
+	assertEquals(__LINE__, 0.0f, 1.0f, 3.0f, v1);
+	assertEquals(__LINE__, -1.0f, 0.0f, 3.0f, v2);
+
+	//RotateAxis
+	v1 = Vector3(3.0f, 0.0f, 1.0f);
+	v2 = rotateAxis(v1, toRadians(90), Vector3(1,0,0));
+	assertEquals(__LINE__, 3.0f, 0.0f, 1.0f, v1);
+	assertEquals(__LINE__, 3.0f, -1.0f, 0.0f, v2);
+
+	v1 = Vector3(0.0f, 3.0f, 1.0f);
+	v2 = rotateAxis(v1, toRadians(90), Vector3(0,1,0));
+	assertEquals(__LINE__, 0.0f, 3.0f, 1.0f, v1);
+	assertEquals(__LINE__, 1.0f, 3.0f, 0.0f, v2);
+
+	v1 = Vector3(0.0f, 1.0f, 3.0f);
+	v2 = rotateAxis(v1, toRadians(90), Vector3(0,0,1));
+	assertEquals(__LINE__, 0.0f, 1.0f, 3.0f, v1);
+	assertEquals(__LINE__, -1.0f, 0.0f, 3.0f, v2);
+
+	//Truncate
+	v1 = Vector3(1.0f, 1.0, 1.0).resize(10);
+	assertEquals(__LINE__, 5.0f, truncate(v1, 5.0f).size());
+	assertEquals(__LINE__, 10.0f, truncate(v1, 15.0f).size());
+
+	//Normalize
+	v1 = Vector3(1, 2, 3);
+	v2 = normalize(v1);
+	assertEquals(__LINE__, 1, 2, 3, v1);
+	assertEquals(__LINE__, 1, v2.size());
+
+	v1 = Vector3(0,0,0);
+	v2 = normalize(v1);
+	assertEquals(__LINE__, 0, v2.size());
+
+	//Reflect
+	v1.set(1.0f, 1.0f, 1.0f);
+	v2 = reflect(v1, Vector3(0,0,1));
+	assertEquals(__LINE__, 1.0f, 1.0f, 1.0f, v1);
+	assertEquals(__LINE__, 1.0f, 1.0f, -1.0f, v2);
 }
