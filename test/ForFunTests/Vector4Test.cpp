02f6f2495fcb4f3238b90cfa4c9dca3d5e652cf1
Vinicius Godoy <vinigodoy@gmail.com>
2012-02-26 16:10:00 -0300

    Tests for Vector4 class (just warnings so far)

---
+++ test/ForFunTests/Vector4Test.cpp
@@ -0,0 +1,10 @@
+#include "Vector4Test.hpp"
+
+#include "assert.hpp"
+#include <Math/Vector4.hpp>
+
+void testVector4()
+{ 
+	reportMissing("Vector4 class");	
+}
+
---
+++ test/ForFunTests/Vector4Test.hpp
@@ -0,0 +1,6 @@
+#if !defined(__VECTOR4_TEST_HPP__)
+#define __VECTOR4_TEST_HPP__
+
+void testVector4();
+
+#endif
