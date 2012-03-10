2e67db6f3ee60b901b3ad2a155116fe2a26e20a8
Vinicius Godoy <vinigodoy@gmail.com>
2012-03-10 15:51:41 -0300

    Changed swap() method name to flip()

--- src/Renderer/SwapChain.cpp
+++ src/Renderer/SwapChain.cpp
@@ -2,19 +2,19 @@
 *
 * COPYRIGHT Vinícius G. Mendonça ALL RIGHTS RESERVED.
 *
-* This software cannot be copied, stored, distributed without  
+* This software cannot be copied, stored, distributed without
 * Vinícius G.Mendonça prior authorization.
 *
-* This file was made available on https://github.com/ViniGodoy/ForFun and it 
-* is free to be restributed or used under Creative Commons license 2.5 br: 
+* This file was made available on https://github.com/ViniGodoy/ForFun and it
+* is free to be restributed or used under Creative Commons license 2.5 br:
 * http://creativecommons.org/licenses/by-sa/2.5/br/
 *
 *******************************************************************************
-* Este software nao pode ser copiado, armazenado, distribuido sem autorização 
+* Este software nao pode ser copiado, armazenado, distribuido sem autorização
 * a priori de Vinícius G. Mendonça
 *
-* Este arquivo foi disponibilizado no site https://github.com/ViniGodoy/ForFun 
-* e esta livre para distribuição seguindo a licença Creative Commons 2.5 br: 
+* Este arquivo foi disponibilizado no site https://github.com/ViniGodoy/ForFun
+* e esta livre para distribuição seguindo a licença Creative Commons 2.5 br:
 * http://creativecommons.org/licenses/by-sa/2.5/br/
 *
 ******************************************************************************/
@@ -30,7 +30,7 @@ SwapChain::SwapChain(int w, int h, bool fullscreen)
 	int flags = SDL_SWSURFACE;
 
 	if (fullscreen) flags |= SDL_FULLSCREEN;
-	SDL_Surface* video = SDL_SetVideoMode(w, h, 32, flags);	
+	SDL_Surface* video = SDL_SetVideoMode(w, h, 32, flags);
 	if (video == NULL)
 		exit(1); //TODO: Replace by exception
 	buffer = new PixelBuffer(video);
@@ -41,8 +41,8 @@ PixelBuffer& SwapChain::backBuffer()
 	return *buffer;
 }
 
-void SwapChain::swap()
-{	
+void SwapChain::flip()
+{
 	SDL_Flip(buffer->surface);
 }
 
