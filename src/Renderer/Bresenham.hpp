ca72cfab4dfd5b989c6c6a6d4e6acf564b25f37c
Vinicius Godoy <vinigodoy@gmail.com>
2012-07-26 22:41:20 -0300

    Added copyright notice.

--- src/Renderer/Bresenham.hpp
+++ src/Renderer/Bresenham.hpp
@@ -1,3 +1,24 @@
+/******************************************************************************
+*
+* COPYRIGHT Vinícius G. Mendonça ALL RIGHTS RESERVED.
+*
+* This software cannot be copied, stored, distributed without  
+* Vinícius G.Mendonça prior authorization.
+*
+* This file was made available on https://github.com/ViniGodoy/ForFun and it 
+* is free to be restributed or used under Creative Commons license 2.5 br: 
+* http://creativecommons.org/licenses/by-sa/2.5/br/
+*
+*******************************************************************************
+* Este software nao pode ser copiado, armazenado, distribuido sem autorização 
+* a priori de Vinícius G. Mendonça
+*
+* Este arquivo foi disponibilizado no site https://github.com/ViniGodoy/ForFun 
+* e esta livre para distribuição seguindo a licença Creative Commons 2.5 br: 
+* http://creativecommons.org/licenses/by-sa/2.5/br/
+*
+******************************************************************************/
+
 #pragma once
 
 #include <vector>
